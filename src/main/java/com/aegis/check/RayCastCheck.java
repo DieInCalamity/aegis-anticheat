c732382783877d8b598d1b4e67941dfef48d700f
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2025-11-09 03:31:55 +0700

    fix imports

--- src/main/java/com/aegis/check/RayCastCheck.java
+++ src/main/java/com/aegis/check/RayCastCheck.java
@@ -11,7 +11,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
-import org.bukkit.RayTraceResult;
+import org.bukkit.util.RayTraceResult;
 
 public class RayCastCheck extends CheckBase {
 
